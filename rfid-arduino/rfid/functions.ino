ba2dff183243b4de7592a04b5625b38c453e8d3c
Vasilis Naskos <vnaskos@gmail.com>
2017-05-07 00:00:18 +0300

    Created code to read and write data using arduino and RFID

---
+++ rfid-arduino/rfid/functions.ino
@@ -0,0 +1,74 @@
+
+       
+int writeBlock(int blockNumber, byte arrayAddress[]) 
+{
+  //this makes sure that we only write into data blocks. Every 4th block is a trailer block for the access/security info.
+  int largestModulo4Number=blockNumber/4*4;
+  int trailerBlock=largestModulo4Number+3;//determine trailer block for the sector
+  if (blockNumber > 2 && (blockNumber+1)%4 == 0){Serial.print(blockNumber);Serial.println(" is a trailer block:");return 2;}//block number is a trailer block (modulo 4); quit and send error code 2
+  Serial.print(blockNumber);
+  Serial.println(" is a data block:");
+  
+  /** authentication of the desired block for access **/
+  byte status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &key, &(mfrc522.uid));
+  //byte PCD_Authenticate(byte command, byte blockAddr, MIFARE_Key *key, Uid *uid);
+  //this method is used to authenticate a certain block for writing or reading
+  //command: See enumerations above -> PICC_CMD_MF_AUTH_KEY_A	= 0x60 (=1100000),		// this command performs authentication with Key A
+  //blockAddr is the number of the block from 0 to 15.
+  //MIFARE_Key *key is a pointer to the MIFARE_Key struct defined above, this struct needs to be defined for each block. New cards have all A/B= FF FF FF FF FF FF
+  //Uid *uid is a pointer to the UID struct that contains the user ID of the card.
+  if (status != MFRC522::STATUS_OK) {
+   Serial.print("PCD_Authenticate() failed: ");
+   Serial.println(mfrc522.GetStatusCodeName(status));
+   return 3;//return "3" as error message
+  }
+  //it appears the authentication needs to be made before every block read/write within a specific sector.
+  //If a different sector is being authenticated access to the previous one is lost.
+
+
+  /** writing the block **/
+        
+  status = mfrc522.MIFARE_Write(blockNumber, arrayAddress, 16);//valueBlockA is the block number, MIFARE_Write(block number (0-15), byte array containing 16 values, number of bytes in block (=16))
+  //status = mfrc522.MIFARE_Write(9, value1Block, 16);
+  if (status != MFRC522::STATUS_OK) {
+   Serial.print("MIFARE_Write() failed: ");
+   Serial.println(mfrc522.GetStatusCodeName(status));
+   return 4;//return "4" as error message
+  }
+  Serial.println("block was written");
+}
+
+
+int readBlock(int blockNumber, byte arrayAddress[]) 
+{
+  int largestModulo4Number=blockNumber/4*4;
+  int trailerBlock=largestModulo4Number+3;//determine trailer block for the sector
+
+  /** authentication of the desired block for access **/
+  byte status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, trailerBlock, &key, &(mfrc522.uid));
+  //byte PCD_Authenticate(byte command, byte blockAddr, MIFARE_Key *key, Uid *uid);
+  //this method is used to authenticate a certain block for writing or reading
+  //command: See enumerations above -> PICC_CMD_MF_AUTH_KEY_A	= 0x60 (=1100000),		// this command performs authentication with Key A
+  //blockAddr is the number of the block from 0 to 15.
+  //MIFARE_Key *key is a pointer to the MIFARE_Key struct defined above, this struct needs to be defined for each block. New cards have all A/B= FF FF FF FF FF FF
+  //Uid *uid is a pointer to the UID struct that contains the user ID of the card.
+  if (status != MFRC522::STATUS_OK) {
+   Serial.print("PCD_Authenticate() failed (read): ");
+   Serial.println(mfrc522.GetStatusCodeName(status));
+   return 3;//return "3" as error message
+  }
+  //it appears the authentication needs to be made before every block read/write within a specific sector.
+  //If a different sector is being authenticated access to the previous one is lost.
+
+
+  /** reading a block **/
+        
+  byte buffersize = 18;//we need to define a variable with the read buffer size, since the MIFARE_Read method below needs a pointer to the variable that contains the size... 
+  status = mfrc522.MIFARE_Read(blockNumber, arrayAddress, &buffersize);//&buffersize is a pointer to the buffersize variable; MIFARE_Read requires a pointer instead of just a number
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print("MIFARE_read() failed: ");
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return 4;//return "4" as error message
+  }
+  //Serial.println("block was read");
+}
---
+++ rfid-arduino/rfid/rfid.ino
@@ -0,0 +1,101 @@
+/*
+ * For demonstration purposes we write a CityItemId to the RFID card
+ * and then we read it back
+ * 
+ * Typical pin layout used:
+ * -----------------------------------------------------------------------------------------
+ *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+ *             Reader/PCD   Uno           Mega      Nano v3    Leonardo/Micro   Pro Micro
+ * Signal      Pin          Pin           Pin       Pin        Pin              Pin
+ * -----------------------------------------------------------------------------------------
+ * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+ * SPI SS      SDA(SS)      10            53        D10        10               10
+ * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+ * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+ * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+ * 
+ */
+
+#include <SPI.h>//include the SPI bus library
+#include <MFRC522.h>//include the RFID reader library
+
+#define SS_PIN 10  //slave select pin
+#define RST_PIN 5  //reset pin
+MFRC522 mfrc522(SS_PIN, RST_PIN); // instatiate a MFRC522 reader object.
+MFRC522::MIFARE_Key key; //create a MIFARE_Key struct named 'key', which will hold the card information
+
+int block=4; //Read Write Block
+byte blockcontent[16] = {"590e1bfe0a975a4d85964909"};//an array with 16 bytes to be written into one of the 64 card blocks is defined
+//byte blockcontent[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};//all zeros. This can be used to delete a block.
+byte readbackblock[18];//This array is used for reading out a block. The MIFARE_Read method requires a buffer that is at least 18 bytes to hold the 16 bytes of a block.
+
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  mfrc522.PCD_Init();
+  Serial.println("Scan a card");
+  
+  // Prepare the security key for the read and write functions - all six key bytes are set to 0xFF from the factory.
+  // Since the cards in the kit are new and the keys were never defined, they are 0xFF
+  
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;//keyByte is defined in the "MIFARE_Key" 'struct' definition in the .h file of the library
+  }
+}
+
+void loop()
+{
+
+  /** Establish connection with a tag/card **/
+        
+  // Look for new cards
+	if ( !mfrc522.PICC_IsNewCardPresent()) {//if PICC_IsNewCardPresent returns 1, a new card has been found and we continue
+		return; //if it did not find a new card, it returns to the start of the loop
+	}
+
+	// Select one of the cards
+	if ( !mfrc522.PICC_ReadCardSerial()) {//if PICC_ReadCardSerial returns 1, the "uid" struct contains the ID of the read card.
+		return;//if it returns a '0' something went wrong and we return to the start of the loop
+	}
+
+  Serial.println("card selected");
+
+  /** Writing and reading on the card **/
+
+  //City Item ID is more than 16 bytes (size of block) so we use 2 of them (#2 and #4)
+  writeBlock(2, {"590e1bfe0a975a4d"});//the blockcontent array is written into the card block
+  writeBlock(4, {"85964909"});
+  //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+         
+  //The 'PICC_DumpToSerial' method 'dumps' the entire MIFARE data block into the serial monitor. Very useful while programming a sketch with the RFID reader...
+  //Notes:
+  //(1) MIFARE cards conceal key A in all trailer blocks, and shows 0x00 instead of 0xFF. This is a secutiry feature. Key B appears to be public by default.
+  //(2) The card needs to be on the reader for the entire duration of the dump. If it is removed prematurely, the dump interrupts and an error message will appear.
+  //(3) The dump takes longer than the time alloted for interaction per pairing between reader and card, i.e. the readBlock function below will produce a timeout if
+  //    the dump is used.
+         
+  //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));//uncomment this if you want to see the entire 1k memory with the block written into it.
+         
+  readBlock(2, readbackblock);//read the block back
+  //Serial.print("read block: ");
+  for (int j=0 ; j<16 ; j++)//print the block contents
+  {
+   Serial.write (readbackblock[j]);//Serial.write() transmits the ASCII numbers as human readable characters to serial monitor
+  }
+
+  //clear read block
+  for(int i=0; i<sizeof(readbackblock); i++) {
+    readbackblock[i] = 0;
+  }
+  
+  readBlock(4, readbackblock);//read the block back
+  for (int j=0 ; j<8 ; j++)//print the block contents
+  {
+   Serial.write (readbackblock[j]);//Serial.write() transmits the ASCII numbers as human readable characters to serial monitor
+  }
+  
+  Serial.println("");
+}
+
+
