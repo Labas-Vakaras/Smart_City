0a3a4acf5a81e6eb606c24832512d34d7b4901e7
Arxa <nikiarch@teiser.gr>
2017-05-06 13:38:03 +0300

    Importing and Exporting Mongo database

--- src/main/java/labasvakaras/smartcity/Main.java
+++ src/main/java/labasvakaras/smartcity/Main.java
@@ -1,9 +1,43 @@
 package labasvakaras.smartcity;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 import static spark.Spark.get;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        /*
+        Database has already one simple document in it
+
+        HOW TO EXPORT DATABASE:
+        type the following command in the desired path location in the shell
+        * NOT 'inside' the mongod shell
+        mongoexport -d smart_city -c city_items -o city_items.json
+
+        HOW TO IMPORT:
+        mongoimport --drop -d smart_city -c city_items city_items.json
+         */
+        final MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost"));
+        final MongoDatabase database = mongoClient.getDatabase("smart_city");
+        final MongoCollection<BasicDBObject> collection = database.getCollection("city_items",BasicDBObject.class);
+
+        /*
+        Getting the first entry in our collection
+         */
+        BasicDBObject data = collection.find().first();
+
+        System.out.println(data.toJson());
+
+
         get("/hello", (req, res) -> "Hello World");
     }
+
+
+
+
 }
